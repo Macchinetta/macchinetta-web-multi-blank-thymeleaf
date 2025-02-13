dbb8aa94668c747eb51b7a0b2c571d5ddbd62f06
Yuta-Koizumi <yuuta.koizumi.hr@hco.ntt.co.jp>
2025-02-13 21:24:09 +0900

    RELEASE 1.8.4.RELEASE

--- create-maven-archetype-mybatis3.sh
+++ create-maven-archetype-mybatis3.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -e
 
-sh change-infra.sh MyBatis3
-sed -i -e "s/macchinetta-multi-web-blank-noorm-thymeleaf/macchinetta-multi-web-blank-thymeleaf/g" pom.xml
-sed -i -e "s/macchinetta-multi-web-blank-noorm-thymeleaf/macchinetta-multi-web-blank-thymeleaf/g" create-maven-archetype.sh
-sed -i -e "s|Web Blank Multi Project (No O/R Mapper)|Web Blank Multi Project|g" pom.xml
-sh create-maven-archetype.sh "$1" "$2"
+DEPLOY=$1
+REPOSITORY=$2
+ORM=MyBatis3
+
+sh create-maven-archetype.sh "$DEPLOY" "$REPOSITORY" "$ORM"
